64da45bd69dc083b7dd5929b5294d2b4d1c3323f
Tilman <tilman@Tilmans-MacBook-Air.local>
2021-03-15 20:53:13 -0700

    Adding State

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  //const [days, setDay] = useState(days)
+  
  const days = props.days.map(day => {
    return <DayListItem 
    key={day.id}
