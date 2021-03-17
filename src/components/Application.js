37f026aa6e9fd9da8fdfd2301390ab67a79c3868
Tilman <tilman@Tilmans-MacBook-Air.local>
2021-03-16 21:58:20 -0700

    add appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,52 @@ import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from 'components/DayList';
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: " Miller Hedy",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Elodie Bouthors",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  }
+];
 const days = [
   {
     id: 1,
