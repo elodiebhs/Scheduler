c2e0362e2e3f99a0f827908f8a2bc3af4eb29ba3
Tilman <tilman@Tilmans-MacBook-Air.local>
2021-03-16 21:45:13 -0700

    Displaying Appointments

--- src/components/Appointment/Index.js
+++ src/components/Appointment/Index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./styles.scss";
 
-export default function Appointment(){
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+
+export default function Appointment(props){
 
 
   return(
-    <article className="appointment">"Test"</article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {props.interview ? 
+      <Show student ={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty/>}
+    </article>
   )
 }
 
