47c71b53471605c6f2623a91d68d287b1f4aaf85
Tilman <tilman@Tilmans-MacBook-Air.local>
2021-03-17 15:25:17 -0700

    Create an effect to make a GET request to /api/days using axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "components/Application.scss";
 import DayList from 'components/DayList';
 import Appointment from "components/Appointment/Index";
@@ -50,26 +50,38 @@ const appointments = [
     }
   }
 ];
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
+
+
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday")
+  
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const URL = '/api/days'
+    axios.get(URL).then(response => {
+      setDays([...response.data]);
+    });
+  }, [days])
 
   return (
     <main className="layout">
