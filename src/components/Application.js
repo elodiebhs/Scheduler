b176f17219d7fb6f06659152a637e35dd394e8b4
Tilman <tilman@Tilmans-MacBook-Air.local>
2021-03-19 12:49:12 -0700

    Implementing the Update when saving an appointment

--- src/components/Application.js
+++ src/components/Application.js
@@ -32,7 +32,24 @@ export default function Application(props) {
     const interview = getInterview(state, appointment.interview);
     
   function bookInterview(id, interview) {
-    console.log(id, interview);
+    //console.log(id, interview);
+    
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+      
+    };
+    setState({
+      ...state,
+      appointments
+    });
+
+
   }
 
   return (
