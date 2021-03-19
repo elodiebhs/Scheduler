b6f047b1ce8fc184a4c485b500837ec910baa7b0
Tilman <tilman@Tilmans-MacBook-Air.local>
2021-03-18 18:50:07 -0700

    setting Custom Hook empty, show, create

--- src/components/Appointment/Index.js
+++ src/components/Appointment/Index.js
@@ -4,15 +4,38 @@ import "./styles.scss";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "components/Appointment/Form";
 
-export default function Appointment(props){
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE ="CREATE";
 
-  return(
+export default function Appointment(props) {
+
+  //When props.interview contains a value, then we want to pass useVisualMode the SHOW mode, if it is empty then we should pass EMPTY.
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+
+  return (
     <article className="appointment">
-      <Header time={props.time}/>
-      {props.interview ? 
-      <Show student ={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty/>}
+      <Header time={props.time} />
+      {/* If we start in the EMPTY mode and call transition(CREATE) then the mode will be changed, and React will render the component. */}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE) } />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {/* When the mode === CREATE we want to show the Form component. */}
+      {mode === CREATE && 
+      (<Form
+       interviewers={[]}
+       onCancel ={back}
+      />)
+      
+      }
     </article>
   )
 }
