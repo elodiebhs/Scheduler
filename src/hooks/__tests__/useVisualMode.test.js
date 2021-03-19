c572822f0c2948646c4fede412461359a6f685f9
Tilman <tilman@Tilmans-MacBook-Air.local>
2021-03-18 17:10:26 -0700

    building hooks and testing

---
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -0,0 +1,63 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+
+import useVisualMode from "hooks/useVisualMode";
+
+const FIRST = "FIRST";
+
+
+test("useVisualMode should initialize with default value", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  expect(result.current.mode).toBe(FIRST);
+});
+
+const SECOND = "SECOND";
+//This test will initialize the mode to FIRST, then transition to SECOND and then check to see what the current value of mode is.
+test("useVisualMode should transition to another mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+});
+
+
+const THIRD = "THIRD";
+//This next test will initialize the mode to FIRST and then transition to SECOND. The first assertion confirms that we made it to SECOND. If we do not include this assertion, then it's possible for the test to pass even if back isn't working. Then, our test will attempt to transition to THIRD. Afterwards, the test will attempt to pop back to the most recent mode, which should be SECOND. After another back, we should find ourselves back at the FIRST mode.
+
+
+test("useVisualMode should return to previous mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.transition(THIRD));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+test("useVisualMode should not return to previous mode if already at initial", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+test("useVisualMode should replace the current mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  // Passing "true" to transition(THIRD, true) says "Transition to THIRD by REPLACING SECOND"
+  act(() => result.current.transition(THIRD, true));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,47 @@
+import { StrictMode } from "react";
+
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  
+  //need to keep track of the history of the modes, so we can go backwards
+  
+
+  const [history, setHistory] = useState([initial]);
+  console.log("history",history)
+  console.log("useState", useState)
+
+  //const historyArray = useState(initial)
+  //const history = historyArray[0]
+  //const setHistory = hisotryArray[1]
+
+  //--TRANSITION ex: SHOW TO EDIT 
+  const transition = function (newMode, replace = false) {
+    //we replace in the history. Instead of adding to the mode we replace the current mode
+    if(replace){
+      //we need to nake a new array, with everyting in the history, get everyting in the array, except the last one, and make a new mode
+      //old history = [a,b,c], new mode = d
+      //new history = [a,b,d]
+      setHistory([...history.slice(0, -1), newMode])
+    } else {
+      setHistory([...history, newMode])
+    }
+  }
+
+  ///----BACK ex : EXIT EDIT MODE take us back SHOW
+  //the history can not be empty
+  const back = () => {
+    if (history.length < 2) {
+      return
+    }
+    //remove last element of the array
+    const newHistory = [...history];
+    newHistory.pop();
+    setHistory(newHistory)
+  }
+  
+  //we want the last history of the array
+  const mode = history[history.length -1]
+
+return { mode, transition, back };
+}
