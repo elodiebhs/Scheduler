3462c0c9b218093c0ac91cedff60e4c7a3da5845
Tilman <tilman@Tilmans-MacBook-Air.local>
2021-03-22 15:53:17 -0700

    add tests

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -23,8 +23,10 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
-  
+    setError("");
     props.onSave(name, interviewer);
+  
+   
   }
 
   return (
