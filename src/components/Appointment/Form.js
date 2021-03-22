4d0e2ca4e3fcba5415206c731aa8039732ed7146
Tilman <tilman@Tilmans-MacBook-Air.local>
2021-03-22 14:40:17 -0700

    create form test and make the Form render with a student name

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -28,9 +28,7 @@ export default function Form(props) {
             type="text"
             placeholder="Enter Student Name"
             onChange={event => setName(event.target.value)}
-          /*
-            This must be a controlled component
-          */
+            data-testid={"student-name-input"}
           />
         </form>
         <InterviewerList interviewers={props.interviewers} interviewer={interviewer} setInterviewer={setInterviewer} />
