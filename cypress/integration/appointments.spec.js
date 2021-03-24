be1544fb9327d7df6db6e66e5772772af8d77e0a
Tilman <tilman@Tilmans-MacBook-Air.local>
2021-03-24 16:32:04 -0700

    create should edit an interview test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,46 +1,62 @@
 describe("Appointments", () => {
 
-  
-// "should book an interview"
+
+  // "should book an interview"
   it("should book an interview", () => {
 
     cy.request("GET", "/api/debug/reset")
 
-  // Visits the root of our web server, confirm that the DOM contains the text "Monday".
-  cy.visit("/");
-  cy.contains("Monday");
-  // Clicks on the "Add" button in the second appointment
-  cy.get("[alt=Add]")
- .first()
- .click();
-  // Enters their name
-  cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
-  // Chooses an interviewer
-  cy.get("[alt='Sylvia Palmer']").click();
-  // Clicks the save button
-  cy.contains("Save").click();
-  // Sees the booked appointment
-  cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-  cy.contains(".appointment__card--show", "Sylvia Palmer");
+    // Visits the root of our web server, confirm that the DOM contains the text "Monday".
+    cy.visit("/");
+    cy.contains("Monday");
+    // Clicks on the "Add" button in the second appointment
+    cy.get("[alt=Add]")
+      .first()
+      .click();
+    // Enters their name
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+    // Chooses an interviewer
+    cy.get("[alt='Sylvia Palmer']").click();
+    // Clicks the save button
+    cy.contains("Save").click();
+    // Sees the booked appointment
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
 
+  });
+
+  // "should edit an interview"
+  it("should edit an interview", () => {
+
+
+    // Visits the root of our web server
+    cy.visit("/");
+    // Clicks the edit button for the existing appointment
+    cy.get("[alt=Edit]")
+    .first()
+    .click({ force: true });
+    // Changes the name and interviewer
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+    // Clicks the save button
+    cy.contains("Save").click();
+    // Sees the edit to the appointment
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
 
   });
 
 
 });
-  
-  
-  
 
-  
+
+
+
+
 
   // "should edit an interview"
 
-  // Visits the root of our web server
-  // Clicks the edit button for the existing appointment
-  // Changes the name and interviewer
-  // Clicks the save button
-  // Sees the edit to the appointment
+
 
   // "should cancel an interview"
 
