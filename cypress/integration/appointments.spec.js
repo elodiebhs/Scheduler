b698e342a899d5808d1f70a5238d9fc48339962e
Tilman <tilman@Tilmans-MacBook-Air.local>
2021-03-24 16:25:07 -0700

    create test should book an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,13 +1,38 @@
+describe("Appointments", () => {
 
-  // "should book an interview"
+  
+// "should book an interview"
+  it("should book an interview", () => {
 
-  // Visits the root of our web server
+    cy.request("GET", "/api/debug/reset")
+
+  // Visits the root of our web server, confirm that the DOM contains the text "Monday".
+  cy.visit("/");
+  cy.contains("Monday");
   // Clicks on the "Add" button in the second appointment
+  cy.get("[alt=Add]")
+ .first()
+ .click();
   // Enters their name
+  cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
   // Chooses an interviewer
+  cy.get("[alt='Sylvia Palmer']").click();
   // Clicks the save button
+  cy.contains("Save").click();
   // Sees the booked appointment
+  cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+  cy.contains(".appointment__card--show", "Sylvia Palmer");
 
+
+  });
+
+
+});
+  
+  
+  
+
+  
 
   // "should edit an interview"
 
