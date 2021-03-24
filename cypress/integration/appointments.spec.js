6665d9191d97e8b860c4a9b555d02ed4e16c942f
Tilman <tilman@Tilmans-MacBook-Air.local>
2021-03-24 16:36:52 -0700

    create test should cancel an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -28,13 +28,12 @@ describe("Appointments", () => {
   // "should edit an interview"
   it("should edit an interview", () => {
 
-
     // Visits the root of our web server
     cy.visit("/");
     // Clicks the edit button for the existing appointment
     cy.get("[alt=Edit]")
-    .first()
-    .click({ force: true });
+      .first()
+      .click({ force: true });
     // Changes the name and interviewer
     cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
     cy.get("[alt='Tori Malcolm']").click();
@@ -44,6 +43,24 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Tori Malcolm");
 
+  });
+
+  it("should cancel an interview", () => {
+
+    //  Visits the root of our web server
+    cy.visit("/");
+    // Clicks the delete button for the existing appointment
+    cy.get("[alt=Delete]")
+      .first()
+      .click({ force: true });
+    // Clicks the confirm button
+    cy.contains("Confirm").click();
+    // Sees that the appointment slot is empty
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+    //confirm the absence of the "Archie Cohen" appointment
+    cy.contains(".appointment__card--show", "Archie Cohen")
+    .should("not.exist");
   });
 
 
