41f16b4bf4627e802484f8e2061f7bcf0ba48eb8
Tilman <tilman@Tilmans-MacBook-Air.local>
2021-03-24 15:08:20 -0700

    add plans for testing

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -6,9 +6,36 @@ describe("Navigation", () => {
   it("should navigate to Tuesday", () => {
     cy.visit("/");
     cy.contains("[data-testid=day]", "Tuesday")
-    .click()
-    .should("have.class", "day-list__item--selected");
+      .click()
+      .should("have.class", "day-list__item--selected");
   });
+
+
+  // "should book an interview"
+
+  // Visits the root of our web server
+  // Clicks on the "Add" button in the second appointment
+  // Enters their name
+  // Chooses an interviewer
+  // Clicks the save button
+  // Sees the booked appointment
+
+
+  // "should edit an interview"
+
+  // Visits the root of our web server
+  // Clicks the edit button for the existing appointment
+  // Changes the name and interviewer
+  // Clicks the save button
+  // Sees the edit to the appointment
+
+  // "should cancel an interview"
+
+  //  Visits the root of our web server
+  // Clicks the delete button for the existing appointment
+  // Clicks the confirm button
+  // Sees that the appointment slot is empty
+
 
 
 
