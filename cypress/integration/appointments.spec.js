fad6ada606e9f08e2b42ae05ca34c585bd4cacb8
Tilman <tilman@Tilmans-MacBook-Air.local>
2021-03-24 15:10:54 -0700

    create appointments.spec.js and plans

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,25 @@
+
+  // "should book an interview"
+
+  // Visits the root of our web server
+  // Clicks on the "Add" button in the second appointment
+  // Enters their name
+  // Chooses an interviewer
+  // Clicks the save button
+  // Sees the booked appointment
+
+
+  // "should edit an interview"
+
+  // Visits the root of our web server
+  // Clicks the edit button for the existing appointment
+  // Changes the name and interviewer
+  // Clicks the save button
+  // Sees the edit to the appointment
+
+  // "should cancel an interview"
+
+  //  Visits the root of our web server
+  // Clicks the delete button for the existing appointment
+  // Clicks the confirm button
+  // Sees that the appointment slot is empty
