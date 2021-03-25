e57be21fa500e6b9f4132a750c96c2299a1b7158
Tilman <tilman@Tilmans-MacBook-Air.local>
2021-03-24 18:46:19 -0700

    small changes

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,16 +1,17 @@
 describe("Appointments", () => {
-
-
-  // "should book an interview"
-  it("should book an interview", () => {
-
-    cy.request("GET", "/api/debug/reset");
-
+  
+    beforeEach(() => {
+      cy.request("GET", "/api/debug/reset");
     // Visits the root of our web server, confirm that the DOM contains the text "Monday".
-    cy.visit("/");
-    cy.contains("Monday");
+      cy.visit("/");
+      cy.contains("Monday");
+     });
+
+     // "should book an interview"
+     it("should book an interview", () => {
+    
     // Clicks on the "Add" button in the second appointment
-    cy.get("[alt=Add]");
+    cy.get("[alt=Add]")
       .first()
       .click();
     // Enters their name
@@ -20,16 +21,14 @@ describe("Appointments", () => {
     // Clicks the save button
     cy.contains("Save").click();
     // Sees the booked appointment
-    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-    cy.contains(".appointment__card--show", "Sylvia Palmer");
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones")
+      .contains(".appointment__card--show", "Sylvia Palmer");
 
   });
 
   // "should edit an interview"
   it("should edit an interview", () => {
 
-    // Visits the root of our web server
-    cy.visit("/");
     // Clicks the edit button for the existing appointment
     cy.get("[alt=Edit]")
       .first()
@@ -47,8 +46,7 @@ describe("Appointments", () => {
 
   it("should cancel an interview", () => {
 
-    //  Visits the root of our web server
-    cy.visit("/");
+
     // Clicks the delete button for the existing appointment
     cy.get("[alt=Delete]")
       .first()
